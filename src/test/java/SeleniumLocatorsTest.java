91c444dc77716d2d28947c26b66fcb85ca10091a
Kras-Dev <red069@mail.ru>
2024-04-06 16:51:37 +0300

    Added SeleniumLocatorsTest.java

--- src/test/java/SeleniumLocatorsTest.java
+++ src/test/java/SeleniumLocatorsTest.java
@@ -4,14 +4,13 @@ import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.Select;
 
-import java.security.Key;
-import java.util.List;
 
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 @Tag("Selenium_Locators")
-public class SeleniumLocators {
+public class SeleniumLocatorsTest {
     private final static String BASE_URL = "https://bonigarcia.dev/selenium-webdriver-java/web-form.html";
     WebDriver driver;
+    Actions actions;
 
     @BeforeEach
     void init(){
@@ -19,16 +18,15 @@ public class SeleniumLocators {
         driver.get(BASE_URL);
     }
 
-//    @AfterAll
-//    void close(){
-//        driver.close();
-//    }
+    @AfterAll
+    void close(){
+        driver.close();
+    }
 
     @Test
     void locatorsExample() throws InterruptedException {
         driver.get(BASE_URL);
         driver.manage().window().fullscreen();
-        driver.manage().window().maximize();
 
         //By.id
         WebElement inputTextForm = driver.findElement(By.id("my-text-id"));
@@ -68,17 +66,16 @@ public class SeleniumLocators {
         //By.cssSelector
         WebElement checkbox = driver.findElement(By.cssSelector("input[type='checkbox'][name='my-check']"));
         checkbox.click();
-        Assertions.assertTrue(!checkbox.isSelected(), "После нажатия флажок должен быть снят");
+        Assertions.assertFalse(checkbox.isSelected(), "После нажатия флажок должен быть снят");
 
         //By.xpath
         WebElement radio = driver.findElement(By.xpath("//input[@type='radio'][@name='my-radio']"));
-        Actions actions = new Actions(driver);
+        actions = new Actions(driver);
         actions.moveToElement(radio).click().perform();
         Assertions.assertTrue(radio.isSelected(),"Радиокнопка должна быть выбрана после нажатия");
 
         //By.linkText
         WebElement linkText = driver.findElement(By.linkText("Return to index"));
-        //Actions actions = new Actions(driver);
         actions.moveToElement(linkText).click().perform();
         Thread.sleep(1000);
         Assertions.assertEquals("https://bonigarcia.dev/selenium-webdriver-java/index.html", driver.getCurrentUrl(),
