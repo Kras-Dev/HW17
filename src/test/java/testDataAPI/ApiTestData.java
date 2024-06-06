4ec447266e71e154f7ac8273b82452e4a3671eb1
Kras-Dev <red069@mail.ru>
2024-06-06 23:53:05 +0300

    Added download, auth tests

--- src/test/java/testDataAPI/ApiTestData.java
+++ src/test/java/testDataAPI/ApiTestData.java
@@ -1,6 +1,10 @@
 package testDataAPI;
 
+import modelsAPI.CartItem;
+import modelsAPI.CartItems;
 import modelsAPI.User;
+
+import java.util.List;
 
 public class ApiTestData {
     public static final User DEFAULT_USER = new User(0, "FPMI_user_1", "firstName",
@@ -19,5 +23,12 @@ public class ApiTestData {
     public static final User TEST = User.builder()
             .id(1)
             .username("test")
+            .build();
+
+    public static final CartItems JEANS_ITEM = CartItems.builder()
+            .items(List.of(new CartItem("0040716474", 1, null)))
+            .build();
+    public static final CartItems POLO_ITEM = CartItems.builder()
+            .items(List.of(new CartItem("0041024928", 1, null)))
             .build();
 }
